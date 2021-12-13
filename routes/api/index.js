274be1df70657594511b7eefa3507e1161fea8eb
Parth Pandey <parthpandey2236@gmail.com>
2021-12-14 02:34:24 +0530

    HospitalApi Completed

--- routes/api/index.js
+++ routes/api/index.js
@@ -7,4 +7,8 @@ router.use('/api/v1',require('./v1/register'));
 router.use('/api/v1',require('./v1/report'));
 router.use('/api/v1',require('./v1/authFailed'));
 
+router.get('/',function(req,res){
+    return res.send("<h1>Running!!!</h1>")
+})
+
 module.exports=router;
